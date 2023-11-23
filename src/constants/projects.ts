4bcb4a32ae9717caa9c0657771d854fca454ba3e
Yuriy <yurii.choboda@gmail.com>
2023-11-23 19:41:37 +0200

    change title and project description

--- src/constants/projects.ts
+++ src/constants/projects.ts
@@ -8,31 +8,31 @@ interface IProject {
 const projects: IProject[] = [
   {
     name: 'Lorian Store',
-    desc: 'cloudStorage',
+    desc: 'full-stack e-commerce store for selling clothes',
     github: 'https://github.com/newento14/clothes-ecom',
     link: 'https://newento14-lorian-store.vercel.app/'
   },
   {
     name: 'MovieBoxd',
-    desc: 'cloudStorage',
+    desc: 'netflix like movie web site',
     github: 'https://github.com/newento14/MovieBoxd',
     link: 'https://newento14-movie-boxd-84a29fe206d9.herokuapp.com/'
   },
   {
     name: 'Cloud storage',
-    desc: 'cloudStorage',
+    desc: 'cloud base storage',
     github: 'https://github.com/newento14/cloud-storage',
     link: ''
   },
   {
     name: 'KeyboardHero',
-    desc: 'cloudStorage',
+    desc: 'a website to check the speed of typing',
     github: 'https://github.com/newento14/type-test',
     link: 'https://type-test-sand.vercel.app/'
   },
   {
     name: 'WeatherApp 🤓',
-    desc: 'cloudStorage',
+    desc: 'web program for displaying weather based on data from the OpenWeather API',
     github: 'https://github.com/newento14/weather-app',
     link: 'https://weather-app-nu-pearl.vercel.app/'
   },
