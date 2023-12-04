08fc15854239b4558d463360348a4b1ad40f6d7b
Yuriy <yurii.choboda@gmail.com>
2023-12-04 12:55:19 +0200

    add technologies to each project

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -15,7 +15,7 @@ export default function Page() {
   return (
     <div className="flex flex-wrap w-full justify-center py-20 px-14 gap-16 sm:gap-28">
       {projects.map((x, i) => (
-        <ProjectCard key={x.name} link={x.link} github={x.github} delay={0.1 + 0.05 * i} name={x.name} desc={x.desc}/>
+        <ProjectCard key={x.name} link={x.link} github={x.github} delay={0.1 + 0.05 * i} name={x.name} desc={x.desc} technologies={x.technologies}/>
       ))}
     </div>
   )
--- src/constants/projects.tsx
+++ src/constants/projects.tsx
@@ -1,3 +1,6 @@
+import Image from "next/image";
+import {Nextjs, React, Redux, Tailwindcss, Typescript, Nodejs, Nestjs, Porstgresql, Sass, Express} from "../../public/technologies";
+
 interface IProject {
   name: string,
   desc: string,
@@ -13,38 +16,90 @@ const projects: IProject[] = [
     github: 'https://github.com/newento14/clothes-ecom',
     link: 'https://newento14-lorian-store.vercel.app/',
     technologies: (
-      <div></div>
+      <>
+        <Image src={Nextjs} alt={'Next.js'} title={'Next.js'} />
+        <Image src={React} alt={'React.js'} title={'React.js'} />
+        <Image src={Redux} alt={'Redux'} title={'Redux'} />
+        <Image src={Typescript} alt={'TypeScript'} title={'TypeScript'} />
+        <Image src={Tailwindcss} alt={'TailwindCSS'} title={'TailwindCSS'} />
+        <Image src={Nodejs} alt={'Node.js'} title={'Node.js'} />
+        <Image src={Nestjs} alt={'Nest.js'} title={'Nest.js'} />
+        <Image src={Porstgresql} alt={'PorstgreSQL'} title={'PorstgreSQL'} />
+      </>
     )
   },
   {
     name: 'MovieBoxd',
     desc: 'netflix like movie web site',
     github: 'https://github.com/newento14/MovieBoxd',
-    link: 'https://newento14-movie-boxd-84a29fe206d9.herokuapp.com/'
+    link: 'https://newento14-movie-boxd-84a29fe206d9.herokuapp.com/',
+    technologies: (
+      <>
+        <Image src={React} alt={'React.js'} title={'React.js'} />
+        <Image src={Redux} alt={'Redux'} title={'Redux'} />
+        <Image src={Typescript} alt={'TypeScript'} title={'TypeScript'} />
+        <Image src={Sass} alt={'SCSS'} title={'SCSS'} />
+        <Image src={Nodejs} alt={'Node.js'} title={'Node.js'} />
+      </>
+    )
   },
   {
     name: 'Gymate',
     desc: 'landing page for gym',
     github: 'https://github.com/newento14/gym-landing-page',
-    link: 'https://gym-landing-page-opal.vercel.app/'
+    link: 'https://gym-landing-page-opal.vercel.app/',
+    technologies: (
+      <>
+        <Image src={Nextjs} alt={'Next.js'} title={'Next.js'} />
+        <Image src={React} alt={'React.js'} title={'React.js'} />
+        <Image src={Redux} alt={'Redux'} title={'Redux'} />
+        <Image src={Typescript} alt={'TypeScript'} title={'TypeScript'} />
+        <Image src={Tailwindcss} alt={'TailwindCSS'} title={'TailwindCSS'} />
+      </>
+    )
   },
   {
     name: 'Cloud storage',
     desc: 'cloud base storage',
     github: 'https://github.com/newento14/cloud-storage',
-    link: ''
+    link: '',
+    technologies: (
+      <>
+        <Image src={React} alt={'React.js'} title={'React.js'} />
+        <Image src={Redux} alt={'Redux'} title={'Redux'} />
+        <Image src={Nodejs} alt={'Node.js'} title={'Node.js'} />
+        <Image src={Express} alt={'Express'} title={'Express'} />
+        <Image src={Porstgresql} alt={'PorstgreSQL'} title={'PorstgreSQL'} />
+      </>
+    )
   },
   {
     name: 'KeyboardHero',
     desc: 'a website to check the speed of typing',
     github: 'https://github.com/newento14/type-test',
-    link: 'https://type-test-sand.vercel.app/'
+    link: 'https://type-test-sand.vercel.app/',
+    technologies: (
+      <>
+        <Image src={React} alt={'React.js'} title={'React.js'} />
+        <Image src={Redux} alt={'Redux'} title={'Redux'} />
+        <Image src={Typescript} alt={'TypeScript'} title={'TypeScript'} />
+        <Image src={Tailwindcss} alt={'TailwindCSS'} title={'TailwindCSS'} />
+      </>
+    )
   },
   {
     name: 'WeatherApp 🤓',
     desc: 'web program for displaying weather based on data from the OpenWeather API',
     github: 'https://github.com/newento14/weather-app',
-    link: 'https://weather-app-nu-pearl.vercel.app/'
+    link: 'https://weather-app-nu-pearl.vercel.app/',
+    technologies: (
+      <>
+        <Image src={React} alt={'React.js'} title={'React.js'} />
+        <Image src={Redux} alt={'Redux'} title={'Redux'} />
+        <Image src={Typescript} alt={'TypeScript'} title={'TypeScript'} />
+        <Image src={Tailwindcss} alt={'TailwindCSS'} title={'TailwindCSS'} />
+      </>
+    )
   },
 ]
 
